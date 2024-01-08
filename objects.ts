e7dc9d5c79d1162e61c62986388aa40a65f0b673
Kapilbabal <155950869+Kapilbabal@users.noreply.github.com>
2024-01-08 08:46:35 +0000

    object as a function parameter

--- objects.ts
+++ objects.ts
@@ -5,3 +5,10 @@ let obj: {name: string, age: number, place: string} = {
 };
 
 console.log(obj);
+
+function obj_parser(obj: {name: string, age: number, place: string}) {
+    console.log(`name: ${obj.name} age: ${obj.age} place: ${obj.place}`);
+
+}
+
+obj_parser(obj);
