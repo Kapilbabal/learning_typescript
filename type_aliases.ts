c85663deb6ac228c5d0797d1b319eaa10a9d106d
Kapilbabal <155950869+Kapilbabal@users.noreply.github.com>
2024-01-08 09:00:43 +0000

    custom type aliases

---
+++ type_aliases.ts
@@ -0,0 +1,13 @@
+type Person = {
+    name: string,
+    age: number,
+    country: string,
+};
+
+const myPerson: Person = {
+    name: "Kapil",
+    age: 16,
+    country: "India",
+};
+
+console.log(myPerson);
